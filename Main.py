eff5b9b503870989afbe3d61ff48f9b6d38561a8
Mr.leonard <97007392+Mrleonard333@users.noreply.github.com>
2022-08-14 15:40:38 -0300

    vM_1.5

--- Main.py
+++ Main.py
@@ -8,12 +8,14 @@ Sysinit = False
 MSGid = 0
 User = str()
 
+# v Inicia a conxão com o database
 connection = mysql.connector.connect(
-    host='localhost',
-    user='root',
+    host='Host',
+    user='User',
     password=r'Senha',
-    database='message_db'
+    database='DataBase'
 )
+# v Ativa o cursor
 cursor = connection.cursor()
 
 print('[Fundacao_Crypto] - [Data_Manager]\n')
@@ -22,18 +24,19 @@ SN = str(input('[S/N] Ja possui uma conta: ')).upper()
 
 if SN == 'S':
     Sysinit = True
-    os.system('cls')
+    os.system('cls') # < Vai apagar as mensagens anteriores no cmd
 
     print('Identifique-se')
     sleep(1)
     User = str(input('Digite o seu usuario: '))
     Pass = str(input('Digite a sua senha: '))
-    Login = f'{User} {Pass}'
+    Login = f'{User} {Pass}' # < Vai guardar o usuario ea senha na variavel [Login]
 
-    cursor.execute('SELECT * FROM users')
+    cursor.execute('SELECT * FROM users') # < Vai pegar todas as informações da tabela
 
+                # v Vai mostrar uma lista com as informações
     for L in cursor.fetchall():
-        Logs = f'{L[1]} {L[2]}'
+        Logs = f'{L[1]} {L[2]}' # < Vai guardar o usuario ea senha da tabela
 
         if Logs == Login:
             print('[Processo de Login feito com sucesso]')
@@ -42,6 +45,7 @@ if SN == 'S':
     
     if TF == False:
         print('[*Usuario nao identificado*]')
+        # v Encerra a conexão do DataBase e o cursor
         cursor.close()
         connection.close()
         exit()
@@ -64,14 +68,15 @@ if SN == 'N':
             print('[*Usuario ja existe*]')
             cursor.close()
             connection.close()
-            exit()
+            exit() # < Vai encerrar o sistema
     
     IDs += 1
+    # v Vai inserir o usuario ea senha na tabela de usuarios [users]
     cursor.execute(f'INSERT INTO users (idusers, username, password) VALUES ({IDs}, "{User}", "{Pass}")')
-    connection.commit()
+    connection.commit() # < Irá salvar as mudanças
 
 os.system('cls')
-if not Sysinit: exit()
+if not Sysinit: exit() # < Se a variavel [System_Initiate] não conter o valor true o sistema não irá encerrar
 
 print('Operacoes disponiveis\n')
 sleep(1)
@@ -86,18 +91,18 @@ sleep(1)
 Option = int(input('\nO que deseja fazer?: '))
 
 os.system('cls')
-if not Option: exit()
+if not Option: exit() # < Se a variavel [Option] for nula osistema irá iniciar
 
 if Option == 1:
     NewPass = str(input('Digite sua nova senha: '))
 
-    cursor.execute(f'UPDATE users SET password = "{NewPass}" WHERE username = "{User}"')
+    cursor.execute(f'UPDATE users SET password = "{NewPass}" WHERE username = "{User}"') # < Ira atualizar os dados do usuario
     connection.commit()
 
     print('[Senha alterada]')
 
 if Option == 2:
-    cursor.execute(f'DELETE FROM users WHERE username = "{User}"')
+    cursor.execute(f'DELETE FROM users WHERE username = "{User}"') # < Vai deletar o dado que tenha o seu usuario
     connection.commit()
 
     print('[Usuario deletado]')
@@ -106,7 +111,7 @@ if Option == 3:
     while True:
         os.system('cls')
 
-        cursor.execute('SELECT idmessage FROM message')
+        cursor.execute('SELECT idmessage FROM message') # < Irá criar uma lista com os IDs das mensagens guardadas
         fetch = cursor.fetchall()
         lenIDS = len(fetch)
 
@@ -119,13 +124,14 @@ if Option == 3:
 
             for msg in cursor.fetchall():
                 MSGid += 1
-
+                
+                # v vai mostrar apenas as dez ultimas mensagens
                 if lenIDS > 10:
                     lenIDS -= 1
                     continue
                 
                 if lenIDS <= 10:
-                    print(f'{MSGid} {msg[1]}: {msg[2]}')
+                    print(f'{msg[1]}: {msg[2]}')
             MSGid += 1
 
         Message = str(input('\n[Exit para sair] Digite uma mensagem: '))
